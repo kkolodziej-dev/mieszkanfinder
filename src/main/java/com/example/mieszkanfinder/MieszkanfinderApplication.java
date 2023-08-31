19b91304d07796871e54fa432e479cf661c8c55d
kkolodziej-dev <frostbolty@gmail.com>
2023-08-31 15:25:46 +0200

    first commit

---
+++ src/main/java/com/example/mieszkanfinder/MieszkanfinderApplication.java
@@ -0,0 +1,13 @@
+package com.example.mieszkanfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MieszkanfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MieszkanfinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mieszkanfinder/MieszkanfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mieszkanfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MieszkanfinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
